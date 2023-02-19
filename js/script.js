cad3f525fbf379e1703d75805bf86b9e7968c9e9
luciaguerra <luciaguerra441@gmail.com>
2023-02-19 01:08:49 -0300

    v1.1.0 - add a button to get an emoji combo

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 let emojis = ["😸", "😺", "😼","😽","😿","😾","🤍","💭","💌","💕","🫶🏻", "🫰🏻", "🧚🏻‍♂️", "🐶", "🐷", "🐩", "🐮", "🐭", "🐁", "🐰", "🐇", "🦔", "🐻‍❄️", "🐥", "🦢", "🕊️", "🦭", "🦋", "🐚", "🌸", "🐞", "🪷", "🌱", "🌿", "🪴", "🥞","🥐", "🍓","🍒","🍥","🍦","☕","🧋","🧁","🎂","🍰","🍮","🍩","🍨","🌙","⭐","☁️","⛈️","🌬️","⛄","🌧️","🌨️","✨","🎀","🎨","🩰","🕯️","📓","✉️","🖇️","🗒️","🪞","🫧","🧺"];
 
 let showEmoji = document.getElementById("show-emoji");
-let getEmoji =document.getElementById("get-emoji");
+let getEmoji = document.getElementById("get-emoji");
+let getEmojiCombo = document.getElementById("get-combo");
 
 getEmoji.addEventListener("click", function(){
     let randomIndex = Math.floor(Math.random() * emojis.length);
 
     showEmoji.textContent = emojis[randomIndex];
+})
+
+getEmojiCombo.addEventListener("click", function(){
+    let randomIndexOne = Math.floor(Math.random() * emojis.length);
+    let randomIndexTwo = Math.floor(Math.random() * emojis.length);
+    let randomIndexThree = Math.floor(Math.random() * emojis.length);
+
+    showEmoji.textContent = emojis[randomIndexOne] + emojis[randomIndexTwo] + emojis[randomIndexThree];
 })
